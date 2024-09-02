86bbb8b493bc524a6e71ff3e42023a8850260c29
Jeong Arm <kjwonmail@gmail.com>
2024-09-02 21:33:08 +0900

    Add Adblock campaign

---
+++ adv_vert.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+	let div = document.getElementById('ZNXMCX');
+	div.style.display = 'block';
+});
---
+++ fake_ad.js
@@ -0,0 +1,4 @@
+var e = document.createElement('div');
+e.id = 'ZNXMCX';
+e.style.display = 'none';
+document.body.appendChild(e);
